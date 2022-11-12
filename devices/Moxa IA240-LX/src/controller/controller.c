27ce12ac93711ec78e7a6f6f32943def3d303b18
Robert <rwallhead@gmail.com>
2022-11-12 17:04:37 +1100

    Basic fill-drain cycle that works pretty well

--- devices/Moxa IA240-LX/src/controller/controller.c
+++ devices/Moxa IA240-LX/src/controller/controller.c
@@ -16,6 +16,10 @@ Factory controller.
 #define PUMP_RUN_ADDRESS 0X0000
 #define VALVE_CTRL_ENABLE_ADDRESS 0X0002
 #define VALVE_OPEN_ADDRESS 0X2021
+#define PRESSURE_HIGH_ADDRESS 0X0000
+
+#define DRAIN_CYCLES 4
+
 
 unsigned short random_byte();
 void set_pump_flag(int clientfd, unsigned short flag);
@@ -23,8 +27,10 @@ void set_valve_ctrl_flag(int clientfd, unsigned short flag);
 void enable_valve_control(int clientfd, unsigned short flag);
 void run_pump(int clientfd, unsigned short flag);
 void open_valve(int clientfd);
+bool pressure_high(int clientfd);
 void write_holding_register(int clientfd, unsigned short address, unsigned short value);
 void write_coil(int clientfd, unsigned short address, bool value);
+bool read_input(int clientfd, unsigned short address);
 
 int main()
 {
@@ -42,17 +48,21 @@ int main()
     // Control.
     while (1)
     {
-        int i = 0;
-        for (i = 0; i < 2; i++) {
+        if (pressure_high(clientfd))
+        {
+            int i = 0;
+            for (i = 0; i < DRAIN_CYCLES; i++)
+            {
+                enable_valve_control(clientfd, valve_ctrl_flag);
+                open_valve(clientfd);
+                sleep(4);
+            }
+        }
+        else
+        {
             enable_valve_control(clientfd, valve_ctrl_flag);
             run_pump(clientfd, pump_flag);
-            sleep(4);
-        }
-
-        for (i = 0; i < 2; i++) {
-            enable_valve_control(clientfd, valve_ctrl_flag);
-            open_valve(clientfd);
-            sleep(4);
+            sleep(2);
         }
     }
 
@@ -89,8 +99,14 @@ void open_valve(int clientfd)
     write_coil(clientfd, VALVE_OPEN_ADDRESS, true);
 }
 
+bool pressure_high(int clientfd)
+{
+    return read_input(clientfd, PRESSURE_HIGH_ADDRESS);
+}
+
 void write_holding_register(int clientfd, unsigned short address, unsigned short value)
 {
+    char buffer[255];
     unsigned char payload[] =
     {
         // Header.
@@ -111,10 +127,12 @@ void write_holding_register(int clientfd, unsigned short address, unsigned short
     };
     
     TCPWrite(clientfd, payload,  12);
+    TCPBlockRead(clientfd, buffer, 255);
 }
 
 void write_coil(int clientfd, unsigned short address, bool value)
 {
+    char buffer[255];
     unsigned char payload[] =
     {
         // Header.
@@ -135,4 +153,36 @@ void write_coil(int clientfd, unsigned short address, bool value)
     };
     
     TCPWrite(clientfd, payload,  12);
+    TCPBlockRead(clientfd, buffer, 255);
+}
+
+bool read_input(int clientfd, unsigned short address)
+{
+    char buffer[255];
+    unsigned char payload[] =
+    {
+        // Header.
+        0X00,                   // Transaction identifier MSB
+        0X01,                   // Transaction identifier LSB
+        0X00,                   // Protocol identifier MSB
+        0X00,                   // Protocol identifier LSB
+        0X00,                   // Message (PDU) length MSB
+        0X06,                   // Message (PDU) length LSB
+
+        // Protocol Data Unit.
+        0X00,                   // Remote unit identifier
+        0X02,                   // Function Code: Read Discrete Input
+        address >> 8,           // Address MSB
+        address & 0X00FF,       // Address LSB
+        0X00,                   // Count of inputs requested MSB
+        0X01,                   // Count of inputs requested LSB
+    };
+
+    TCPWrite(clientfd, payload,  12);
+
+    int bytes = 0;
+    bytes = TCPBlockRead(clientfd, buffer, 255);
+
+    // Last value is the read bit.
+    return buffer[bytes - 1] == 0X01;
 }
